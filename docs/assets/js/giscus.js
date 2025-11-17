0c6e904fc0bbabaab2b19bc6e8c7be14056526e3
MaxXSoft <x@MaxXSoft.net>
2025-11-17 22:43:53 +0800

    (giscus) supported toggling dark/light themes

--- docs/assets/js/giscus.js
+++ docs/assets/js/giscus.js
@@ -1,11 +1,34 @@
 const giscusPlugin = (hook, vm) => {
   hook.ready(() => {
+    // Move giscus container to content area.
     const content = document.querySelector('.content')
     const giscusContainer = document.querySelector(`.giscus`)
     if (content && giscusContainer) {
       content.appendChild(giscusContainer)
     }
+    // Add theme toggle event.
+    const themeToggle = document.getElementById('docsify-darklight-theme')
+    if (themeToggle) {
+      themeToggle.addEventListener('click', () => toggleGiscusTheme(themeToggle))
+      toggleGiscusTheme(themeToggle)
+    }
   })
+}
+
+const toggleGiscusTheme = (toggle) => {
+  const iframe = document.querySelector('.giscus-frame')
+  if (iframe) {
+    // Check if dark mode is enabled.
+    const isDark = toggle.getAttribute('data-link-title') === 'dark'
+    const theme = isDark ? 'dark_dimmed' : 'light'
+    // Replace `theme` parameter in iframe src.
+    const src = iframe.getAttribute('src')
+    const newSrc = src.replace(
+      /theme=[^&]*/,
+      `theme=${theme}`
+    )
+    iframe.setAttribute('src', newSrc)
+  }
 }
 
 $docsify.plugins.push(giscusPlugin)
